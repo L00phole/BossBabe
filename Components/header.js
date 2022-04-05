df8c56a5d9191cfbb28d64dab202e0b0bc3d3da5
denjennie <denjennie@live.se>
2022-04-05 16:35:25 +0200

    header, link och mer bilder

--- Components/header.js
+++ Components/header.js
@@ -1,13 +1,18 @@
-export default function Header(){
+import Link from 'next/link'
+
+export default function Header() {
   return (
     <div>
       {
         ['header'].map((path) => {
           return (
             <div key={path}>
-              <img src={`/${path}.jpg`} alt='header' style={{ width: '100%', height: '70%'}} />
-
+              <Link href='/home'>
+          <h1>BOSSBABE</h1>
+        </Link>
+              <img src={`/${path}.jpg`} alt='header' style={{ width: '100vw', height: 'auto' }} />
             </div>
+            
           )
         })
       }
