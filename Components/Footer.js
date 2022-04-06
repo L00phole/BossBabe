10aa1e3d35eb88b7e812c620dc6507bdc045f5ce
Michele <vilda1002@gmail.com>
2022-04-06 16:49:19 +0200

    foooooterrr

--- Components/Footer.js
+++ Components/Footer.js
@@ -3,44 +3,52 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <>
-      <div className=" mb-5 ">
-        <div className="text-center ">
-          <h1 className="font-bold text-xl cursor-pointer">
-            BOSS<span className="text-yellow-400">BABE</span>
-          </h1>{" "}
-          <br />
-          <p className="font-bold text-l ">Work it like a queen</p>
+      <div className="text-center m-2 pb-2">
+        <h1 className="font-bold text-xl cursor-pointer">
+          BOSS<span className="text-yellow-400">BABE</span>
+        </h1>{" "}
+        <br />
+        <p className="font-bold text-l ">Work it like a queen</p>
+      </div>
+      <div className=" flex justify-between shadow-sm max-w-xl mx-auto z-10 mb-2 pb-10">
+        <div className="mx-20">
+          <h2>Shopping</h2>
+          <ul>
+            <li>Nyheter</li>
+            <li>Bästsäljare</li>
+            <li>Outlet</li>
+            <li>Alla produkter</li>
+          </ul>
         </div>
-        <div className="columns-3 content-center ">
-          <div>
-            <h2>Shopping</h2>
-            <ul>
-              <li>Nyheter</li>
-              <li>Bästsäljare</li>
-              <li>Outlet</li>
-              <li>Alla produkter</li>
-            </ul>
-          </div>
-          <div>
-            <h2>Om oss</h2>
-            <ul>
-              <li></li>
-              <li>Bästsäljare</li>
-              <li>Outlet</li>
-              <li>Alla produkter</li>
-            </ul>
-          </div>
-          <div>
-            <h2>Shopping</h2>
-            <ul>
-              <li>Nyheter</li>
-              <li>Bästsäljare</li>
-              <li>Outlet</li>
-              <li>Alla produkter</li>
-            </ul>
-          </div>
+        <div className="mx-10">
+          <h2>Om oss</h2>
+          <ul>
+            <li></li>
+            <li>Bästsäljare</li>
+            <li>Outlet</li>
+            <li>Alla produkter</li>
+          </ul>
+        </div>
+        <div className="mx-10">
+          <h2>Shopping</h2>
+          <ul>
+            <li>Nyheter</li>
+            <li>Bästsäljare</li>
+            <li>Outlet</li>
+            <li>Alla produkter</li>
+          </ul>
+        </div>
+        <div className="mx-10">
+          <h2>Shopping</h2>
+          <ul>
+            <li>Nyheter</li>
+            <li>Bästsäljare</li>
+            <li>Outlet</li>
+            <li>Alla produkter</li>
+          </ul>
         </div>
       </div>
+
       <div className="font-bold text-l mb-0 text-center ">
         <a
           href="https://jennie-michele.0.capacedev.se/"
