1ed124fce8dba5c3bd3a3c42ed1742989babe928
Michele <vilda1002@gmail.com>
2022-04-06 16:02:04 +0200

    footer additions

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,34 +1,64 @@
- import styles from '../styles/Home.module.css'
-import Image from 'next/image'
-
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <>
-  
-      <div className='grid gap-10 mb-5'>
-        <div className='text-center '>
-            <h1 className="font-bold text-xl cursor-pointer">
-                  BOSS<span className="text-yellow-400">BABE</span>
-
-                  </h1> <br />
-            <p className="font-bold text-l">Work it like a queen</p>
-            </div>
+      <div className=" mb-5 ">
+        <div className="text-center ">
+          <h1 className="font-bold text-xl cursor-pointer">
+            BOSS<span className="text-yellow-400">BABE</span>
+          </h1>{" "}
+          <br />
+          <p className="font-bold text-l ">Work it like a queen</p>
         </div>
-          <div className="font-bold text-l mb-0 text-center ">
+        <div className="columns-3 content-center ">
+          <div>
+            <h2>Shopping</h2>
+            <ul>
+              <li>Nyheter</li>
+              <li>Bästsäljare</li>
+              <li>Outlet</li>
+              <li>Alla produkter</li>
+            </ul>
+          </div>
+          <div>
+            <h2>Om oss</h2>
+            <ul>
+              <li></li>
+              <li>Bästsäljare</li>
+              <li>Outlet</li>
+              <li>Alla produkter</li>
+            </ul>
+          </div>
+          <div>
+            <h2>Shopping</h2>
+            <ul>
+              <li>Nyheter</li>
+              <li>Bästsäljare</li>
+              <li>Outlet</li>
+              <li>Alla produkter</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="font-bold text-l mb-0 text-center ">
         <a
           href="https://jennie-michele.0.capacedev.se/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by Jennie och Michele {' '}
+          Powered by Jennie och Michele{" "}
           <span>
-            <Image src="/favicon.ico" alt="Bossbabe Logo" width={13} height={13} />
+            <Image
+              src="/favicon.ico"
+              alt="Bossbabe Logo"
+              width={13}
+              height={13}
+            />
           </span>
         </a>
       </div>
-
     </>
-  )
-}
+  );
+};
 export default Footer;
