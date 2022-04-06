cdf439602de3a8ea818ab012405c647fa6006d39
denjennie <denjennie@live.se>
2022-04-06 09:44:17 +0200

    Header

---
+++ Components/navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Transition } from "@headlessui/react"
+
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="shadow-sm fixed w-full z-10">
+        <div classname="w-full">
+          <div className="flex items-center h-20 w-full">
+            <div className="flex items items-center mx-20 justify-betweeen w-full">
+              <div className="flex justify-center items-center flex-shrink-0">
+                <h1 className="font-bold text-xl cursor-pointer">
+                  BOSS<span className="text-blue-500">BABE</span>
+
+                  </h1>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      
+      </nav>
+    </div>
+  )
+  
+}
