51e97e12c9052aa8315c1f37d59becfe6b173f54
Michele <vilda1002@gmail.com>
2022-04-07 16:27:32 +0200

    fixade layout.js

---
+++ layout/layout.js
@@ -0,0 +1,19 @@
+import Navbar from "../Components/navbar";
+import Footer from "../Components/Footer";
+import Head from "next/head";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <div className="content">
+        <Head>
+          <title>Bossbabe</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </>
+  );
+}
