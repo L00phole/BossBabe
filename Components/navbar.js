a18788ba7140659c91e4126dd3e782f93d216e6e
denjennie <denjennie@live.se>
2022-04-08 13:21:09 +0200

    reasponsive navbar

--- Components/navbar.js
+++ Components/navbar.js
@@ -1,93 +1,117 @@
 import Link from "next/link";
 import React from "react";
+import { useState } from "react";
+
+function NavLink({ to, children }) {
+  return (
+    <a href={to} className={`mx-4`}>
+      {children}
+    </a>
+  );
+}
+
+function MobileNav({ open, setOpen }) {
+  return (
+    <div
+      className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${
+        open ? "-translate-x-0" : "-translate-x-full"
+      } transition-transform duration-300 ease-in-out filter drop-shadow-md `}
+    >
+      <div className="flex items-center justify-center filter drop-shadow-md bg-white h-20">
+        {" "}
+        <h1 className="font-bold text-3xl cursor-pointer">
+          <a className="text-2xl font-semibold" href="/">
+            <span className="text-slate-800">BOSS</span>
+            <span className="text-yellow-400">BABE</span>
+          </a>
+        </h1>
+      </div>
+      <div className="flex flex-col ml-4">
+        <a
+          className="text-xl font-medium my-4 hover:text-yellow-400"
+          href="/om-oss"
+          onClick={() =>
+            setTimeout(() => {
+              setOpen(!open);
+            }, 100)
+          }
+        >
+          Om oss
+        </a>
+        <a
+          className="text-xl font-medium my-4 hover:text-yellow-400"
+          href="/rea"
+          onClick={() =>
+            setTimeout(() => {
+              setOpen(!open);
+            }, 100)
+          }
+        >
+          Rea
+        </a>
+        <a
+          className="text-xl font-medium my-4 hover:text-yellow-400"
+          href="/shoppa-nu"
+          onClick={() =>
+            setTimeout(() => {
+              setOpen(!open);
+            }, 100)
+          }
+        >
+          Shoppa loss
+        </a>
+      </div>
+    </div>
+  );
+}
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="md:fixed fluid w-full bg-white p-5 z-50 ">
-      <nav className=" md:flex ljustify-between shadow-sm  mx-auto z-10">
-        <div className="md:flex items-center">
-          <h1 className="font-bold text-3xl cursor-pointer">
-            <Link href="/">
-              <a>
-                <span className="text-slate-800">BOSS</span>
-                <span className="text-yellow-400">BABE</span>
-              </a>
-            </Link>
-          </h1>
-          <div className="ml-10 flex items-baseline space-x-4">
-            <Link href="/om-oss">
-              <a className="cursor-pointer text-black px-3 py-2 font-bold hover:text-yellow-400">
-                Om Oss
-              </a>
-            </Link>
-            <Link href="/rea">
-              <a className="cursor-pointer text-black px-3 py-2 font-bold hover:text-yellow-400">
-                Rea
-              </a>
-            </Link>
-            <Link href="/shoppa-nu">
-              <a className="cursor-pointer text-black px-3 py-2 font-bold hover:text-yellow-400">
-                Shoppa loss
-              </a>
-            </Link>
-          </div>
-        </div>
-
-        <div className="flex items-center ">
-          <a className="px-3">
-            <svg
-              className="cursor-pointer  h-6 w-6 "
-              xmlns="http://www.w3.org/2000/svg"
-              enableBackground="new 0 0 24 24"
-              height="24px"
-              viewBox="0 0 24 24"
-              width="24px"
-              fill="#000000"
-            >
-              <g>
-                <path d="M0,0h24v24H0V0z" fill="none" />
-              </g>
-              <g>
-                <path d="M12,4.81L12,19c-3.31,0-6-2.63-6-5.87c0-1.56,0.62-3.03,1.75-4.14L12,4.81 M12,2L6.35,7.56l0,0C4.9,8.99,4,10.96,4,13.13 C4,17.48,7.58,21,12,21c4.42,0,8-3.52,8-7.87c0-2.17-0.9-4.14-2.35-5.57l0,0L12,2z" />
-              </g>
-            </svg>
+    <nav className="flex filter drop-shadow-md bg-white px-4 py-4 h-20 items-center">
+      <MobileNav open={open} setOpen={setOpen} />
+      <div className="w-3/12 flex items-center">
+        <a className="text-2xl font-semibold" href="/">
+          <a>
+            <span className="text-slate-800">BOSS</span>
+            <span className="text-yellow-400">BABE</span>
           </a>
-
-          <Link href="/profil">
-            <a className="px-3 ">
-              <svg
-                className="cursor-pointer h-8 w-8 "
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 0 24 24"
-                width="24px"
-                fill="#000000"
-              >
-                <path d="M0 0h24v24H0V0z" fill="none" />
-                <path d="M12 6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2m0 9c2.7 0 5.8 1.29 6 2v1H6v-.99c.2-.72 3.3-2.01 6-2.01m0-11C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z" />
-              </svg>
-            </a>
-          </Link>
-          <Link href="/my-cart">
-            <a className="px-3">
-              <svg
-                className="cursor-pointer  h-7 w-7"
-                xmlns="http://www.w3.org/2000/svg"
-                enableBackground="new 0 0 24 24"
-                height="24px"
-                viewBox="0 0 24 24"
-                width="24px"
-                fill="#000000"
-              >
-                <g>
-                  <rect fill="none" height="24" width="24" />
-                  <path d="M18,6h-2c0-2.21-1.79-4-4-4S8,3.79,8,6H6C4.9,6,4,6.9,4,8v12c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2V8C20,6.9,19.1,6,18,6z M12,4c1.1,0,2,0.9,2,2h-4C10,4.9,10.9,4,12,4z M18,20H6V8h2v2c0,0.55,0.45,1,1,1s1-0.45,1-1V8h4v2c0,0.55,0.45,1,1,1s1-0.45,1-1V8 h2V20z" />
-                </g>
-              </svg>
-            </a>
-          </Link>
+        </a>
+      </div>
+      <div className="w-9/12 flex justify-end items-center">
+        <div
+          className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          <span
+            className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
+              open ? "rotate-45 translate-y-3.5" : ""
+            }`}
+          />
+          <span
+            className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${
+              open ? "w-0" : "w-full"
+            }`}
+          />
+          <span
+            className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
+              open ? "-rotate-45 -translate-y-3.5" : ""
+            }`}
+          />
         </div>
-      </nav>
-    </div>
+
+        <div className="hidden md:flex cursor-pointer text-black px-3 py-2 font-bold hover:text-yellow-400">
+          <NavLink to="/om-oss">Om Oss</NavLink>
+        </div>
+        <div className="hidden md:flex cursor-pointer text-black px-3 py-2 font-bold hover:text-yellow-400">
+          <NavLink to="/rea">Rea</NavLink>
+        </div>
+        <div className="hidden md:flex cursor-pointer text-black px-3 py-2 font-bold hover:text-yellow-400">
+          <NavLink to="/shoppa-nu">Shoppa loss</NavLink>
+        </div>
+      </div>
+    </nav>
   );
 }
