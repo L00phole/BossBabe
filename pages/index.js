5e8db716fd6bd85b9cf9db8410d06cd002650976
denjennie <denjennie@live.se>
2022-04-12 16:09:44 +0200

    bytt header

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,14 @@ import Image from "next/image";
 
 export default function Home() {
   const image =
-    "https://images.unsplash.com/photo-1583454110551-21f2fa2afe61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80";
+    "https://images.unsplash.com/photo-1574680088814-c9e8a10d8a4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80";
   const obj = {
     image:
-      "https://images.unsplash.com/photo-1583454110551-21f2fa2afe61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      "https://images.unsplash.com/photo-1574680088814-c9e8a10d8a4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
   };
 
   return (
-    <div className="mt-70 ">
+    <div className="mt-[80px] md:mt-[70px] ">
       <div className=" relative h-[350px]  w-fill md:h-[600px] lg:h-[1000px] ">
         {!obj?.image ? null : (
           <Image
